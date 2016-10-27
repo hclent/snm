e8f2833c1b1dc4c1772b95b9ee10e4f05c3dd076
Jack L <mwli>
2016-10-27 14:29:40 -0700

    normalization cont.

--- realDataPca.py
+++ realDataPca.py
@@ -13,41 +13,47 @@ np.set_printoptions(precision=4)
 
 path = 'ks/cleaned/*.ks'
 files = glob(path)
-genomes = [f.split('/')[-1].split('.')[0].split('_') for f in files]
-genomes = set(np.array(genomes).ravel())
-genomes = [int(g) for g in genomes]
+genomeIDs = [f.split('/')[-1].split('.')[0].split('_') for f in files]
+genomeIDs = set(np.array(genomeIDs).ravel())
+genomeIDs = [int(g) for g in genomeIDs]
 
-print len(genomes)
+print genomeIDs
 
+x = pd.DataFrame(columns=genomeIDs, index=genomeIDs)
 
-##for fileName in sorted(files):
-##    print '_'*60
-##    print fileName
-##    img = imread(fileName)
-##    img = gray(img)
-##    a,b = fileName.split('/')[-1].split('.')[0].split('-')
-##    a,b = ord(a)-65, ord(b)-65
-##    if img[0,0] < 10:
-##        img = 255-img
-##    KS = img/1.0
-##    sigma = 1
-##    ki = kernel(KS, sigma)
-##    x[a][b] = ki
-##    x[b][a] = ki
-##
-##
-##print x
-##pca = PCA(n_components=2)
-##pca.fit(x)
-##print 'pca ratio', pca.explained_variance_ratio_
-##
-##fig = plt.figure()
-##ax = fig.add_subplot(121, projection = '3d')
-##ax.scatter(x[:,0],x[:,1],x[:,2], color = 'bbrrr')
-##ax = fig.add_subplot(122)
-##x = pca.transform(x)
-##print x
-##ax.scatter(x[:,0],x[:,1], color='bbrrr')
-####ax.axis('square')
-##
-##plt.show()
+for f in files:
+    print '_'*60
+    print f
+    genA, genB = f.split('/')[-1].split('.')[0].split('_')
+    genA, genB = int(genA), int(genB)
+    ki = kernel(f, sigma=1)
+    print ki
+    x[genA][genB] = ki
+    x[genB][genA] = ki
+
+genomeInfo = pd.read_csv('ks/geneCount.txt',
+                         index_col = 'id')
+
+for i in genomeIDs:
+    x[i][i] = 1
+    
+print x
+
+pca = PCA(n_components=2)
+pca.fit(x)
+print 'pca ratio', pca.explained_variance_ratio_
+
+fig = plt.figure()
+ax = fig.add_subplot(121, projection = '3d')
+ax.scatter(x.iloc[:,0],x.iloc[:,1],x.iloc[:,2], color = 'bbrr')
+ax = fig.add_subplot(122)
+xCap = pca.transform(x)
+print xCap
+ax.scatter(xCap[:,0],xCap[:,1], color='bbrr')
+ax.axis('square')
+
+names = genomeInfo['name']
+for pt,gID in zip(xCap, genomeIDs):
+    ax.text(pt[0],pt[1],names[gID])
+    
+plt.show()
